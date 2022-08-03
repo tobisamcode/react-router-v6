4f09fa9ad29f0dd225db36480f99098aefaa8e7d
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-03 23:41:27 +0100

    router 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./page/About";
 import Home from "./page/Home";
+import Products from "./page/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<h2>about</h2>} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
