eea2787da7d1adf394c7a870d415f5fe6baf1fee
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 00:40:15 +0100

    router 6

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,14 @@
-export default function NavBar() {
-  return <div>NavBar</div>;
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/products">Products</Link>
+    </nav>
+  );
 }
+
+export default NavBar;
