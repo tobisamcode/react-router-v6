045364bdf1ef3b6facfe3d6c253d15fb1f88e3a3
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 00:50:49 +0100

    router 6

--- src/components/StyledNavbar.js
+++ src/components/StyledNavbar.js
@@ -4,9 +4,24 @@ import { NavLink } from "react-router-dom";
 function StyledNavbar() {
   return (
     <nav className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/products">Products</NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/about"
+      >
+        About
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? "link active" : "link")}
+        to="/products"
+      >
+        Products
+      </NavLink>
     </nav>
   );
 }
