e35b4c806424a076a6ad5cd26176cd01a46c2f04
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 00:03:34 +0100

    router 6

--- src/page/About.js
+++ src/page/About.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function About() {
   return (
-    <section>
+    <section className="section">
       <h2>About</h2>
+      <Link to="/" className="btn">
+        Back Home
+      </Link>
     </section>
   );
 }
