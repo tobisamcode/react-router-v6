5f5ac8ffa6f1b993118d82f0bca8df422c464748
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 00:40:11 +0100

    router 6

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import About from "./page/About";
 import Error from "./page/Error";
 import Home from "./page/Home";
 import Products from "./page/Products";
+import SharedLayout from "./page/SharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>Our Navbar</nav>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
-      <footer>our footer</footer>
     </BrowserRouter>
   );
 }
