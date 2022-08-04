c735fe739b09da07f5a939b7b63d248cd87a2f24
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 02:00:04 +0100

    router 6

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,53 @@
-export default function Login() {
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login({ setUser }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if (!name || !email) return;
+    setUser({ name: name, email: email });
+    navigate("/dashboard");
+  };
+
   return (
     <section className="section">
-      <h2>login</h2>
+      <form className="form" onSubmit={handleSubmit}>
+        <h5>login</h5>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            email
+          </label>
+          <input
+            type="email"
+            className="form-input"
+            id="name"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          login
+        </button>
+      </form>
     </section>
   );
 }
+
+export default Login;
