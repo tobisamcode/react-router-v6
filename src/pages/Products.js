336fd5e43b4395a1c968266b8bcad8d090bc2766
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 01:24:44 +0100

    router 6

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import products from "../data";
+
 function Products() {
   return (
     <section className="section">
       <h2>Products</h2>
+      <div className="products">
+        {products.map(product => {
+          return (
+            <article key={product.id}>
+              <h5>
+                {product.name}
+              </h5>
+              <Link to={`/products/${product.id}`} className="btn">
+                More info
+              </Link>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 }
