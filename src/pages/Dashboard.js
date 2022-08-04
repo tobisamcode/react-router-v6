d7aed2a391ecc068866169a3269ee8703c0a73f6
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-08-04 02:00:01 +0100

    router 6

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,11 @@
-export default function Dashboard() {
+import React from "react";
+
+function Dashboard({user}) {
   return (
     <section className="section">
-      <h2>Dashboard</h2>
+      <h4>welcome, { user?.name }</h4>
     </section>
   );
 }
+
+export default Dashboard;
